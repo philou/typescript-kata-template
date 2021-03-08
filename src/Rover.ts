5c13e59ef19eda4e4ed7551a091554dd6c3f8870
Philippe Bourgau <philippe.bourgau@gmail.com>
2021-03-08 10:43:01 +0100

    Working

--- src/Rover.ts
+++ src/Rover.ts
@@ -3,5 +3,5 @@ import { Direction } from "./Direction";
 export class Rover {
     public readonly x = 0;
     public readonly y = 0;
-  public readonly direction = Direction.North;
+    public readonly direction = Direction.North;
 }

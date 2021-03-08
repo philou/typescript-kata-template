302a0bb09ab23836fd822f540eebb055cd4122a2
Philippe Bourgau <philippe.bourgau@gmail.com>
2021-03-08 10:40:55 +0100

    Working

--- test/Rover.spec.ts
+++ test/Rover.spec.ts
@@ -1,9 +1,11 @@
 import { Rover } from "../src/Rover";
 
 describe('Rover', () => {
-  it('should start at origin', () => {});
+  it('should start at origin', () => {
     let rover = new Rover();
 
     expect(rover.x).toBe(0);
     expect(rover.y).toBe(0);
+
+  });
 });

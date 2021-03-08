914a9e30f34070fc43a63f0d6aa515317f8fafb5
Philippe Bourgau <philippe.bourgau@gmail.com>
2021-03-08 10:31:09 +0100

    Working

--- test/Kata.spec.ts
+++ test/Kata.spec.ts
@@ -3,4 +3,7 @@ import { Rover } from "../src/Rover";
 describe('Rover', () => {
   it('should start at origin', () => {});
     let rover = new Rover();
+
+    expect(rover.x).toBe(0);
+    expect(rover.y).toBe(0);
 });
